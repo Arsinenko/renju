176ae97f1b371b5d457420df400b5cf89ffc1e3d
Kim Arsinenko <kimp3061@gmail.com>
2024-01-03 23:19:59 +0300

    Update renju.py
    добавлена функция для вывода текста на экран. сообщение о завершении игры.

--- renju.py
+++ renju.py
@@ -1,4 +1,15 @@
 import pygame
+import time
+
+
+def msg(text: str):
+    # вывод сообщения на экран
+    screen.fill(pygame.Color("black"))
+    text = pygame.font.Font(None, 36).render(text, True, pygame.Color("white"))
+    text_rect = text.get_rect(center=screen.get_rect().center)
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+    pygame.time.delay(2000)
 
 
 def clicked(x_pos: int, y_pos: int) -> list:
@@ -9,7 +20,7 @@ def clicked(x_pos: int, y_pos: int) -> list:
                                                                                                    j * cell_size + 2 + 35):
                 board[i][j] = 'X' if first_player else "O"
 
-                return [i, j]   # возвращаем элемент массива
+                return [i, j]  # возвращаем элемент массива
 
 
 def print_board():
@@ -24,7 +35,7 @@ def print_board():
                 screen.blit(O_image, (i * cell_size + 2, j * cell_size + 2))
 
 
-def get_sign(board: list, x: int, y: int):
+def get_sign(board: list, x: int, y: int) -> str:
     # проверка на выход за приделы массива
     if x > 14 or y > 14 or x < 0 or y < 0:
         return '.'
@@ -51,14 +62,14 @@ def win_line(line: list) -> bool:
 
 pygame.init()
 
-RES = 600   # разрешение экрана
+RES = 600  # разрешение экрана
 cell_size = 39  # размер клетки
 screen = pygame.display.set_mode((RES, RES))
 
 pygame.display.set_caption("GUI")
 icon = pygame.transform.scale(pygame.image.load("images/icon.png"), (50, 50))  # иконка для приложения
 pygame.display.set_icon(icon)
-board = [['.' for _ in range(15)] for _ in range(15)]   # формирование игрового поля в виде массива
+board = [['.' for _ in range(15)] for _ in range(15)]  # формирование игрового поля в виде массива
 black = pygame.Color("black")
 
 # избражение крестика
@@ -75,8 +86,8 @@ while not game_over:
             # проверка закрытия приложения
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x_pos, y_pos = event.pos    # координаты клика мыши
-            button = event.button   # назначение кнопки
+            x_pos, y_pos = event.pos  # координаты клика мыши
+            button = event.button  # назначение кнопки
             x = clicked(x_pos, y_pos)[0]
             y = clicked(x_pos, y_pos)[1]
             # линии в которых могут быть выигрышные комбинации
@@ -86,6 +97,8 @@ while not game_over:
             diagonal2 = [get_sign(board, x + i, y - i) for i in range(-4, 5)]
             # проверка на победу
             if win_line(vertical) or win_line(horizontal) or win_line(diagonal) or win_line(diagonal2):
+                msg("Game over!!!")
+                time.sleep(5)
                 game_over = True
             first_player = not first_player
 
